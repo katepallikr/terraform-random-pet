a9f37c55adfb4d769915970f1f9fb7831176b6fa
Carl Javier <carl.javier@hashicorp.com>
2021-11-17 21:22:49 +1100

    demonstrate templatefile()

--- main.tf
+++ main.tf
@@ -5,3 +5,9 @@ resource "random_string" "random" {
 resource "random_pet" "nickname" {
   length = var.string_length
 }
+
+# Will generate a index.html file which can be viewed by a local browser.
+resource "local_file" "html" {
+  filename = "./index.html"
+  content = templatefile("${path.module}/templates/index.html.tpl", { nickname   = random_pet.nickname.id })
+}
--- versions.tf
+++ versions.tf
@@ -6,5 +6,9 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.0.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0.0"
+    }
   }
 }
