d7514b590f2d6a757ea839ee1bcb53ad06fd9ecc
Carl Javier <carl.javier@hashicorp.com>
2021-11-17 14:40:55 +1100

    Add descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "random_string_output" {
-  value = random_string.random.id
+  description = "Random String generated"
+  value       = random_string.random.id
 }
 
 output "random_pet_nickname" {
-    value = random_pet.nickname.id
+  description = "Random Pet Nickname generated"
+  value       = random_pet.nickname.id
 }
 
 
